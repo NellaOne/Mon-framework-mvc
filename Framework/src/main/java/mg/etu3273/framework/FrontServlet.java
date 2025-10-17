98ccdae719298be9d7dd939672e3442e59f9a335
OrnellaMarion <168301329+NellaOne@users.noreply.github.com>
2025-10-17 22:50:54 +0300

    commit3

--- Framework/src/main/java/mg/etu3273/framework/FrontServlet.java
+++ Framework/src/main/java/mg/etu3273/framework/FrontServlet.java
@@ -24,6 +24,11 @@ public class FrontServlet extends HttpServlet {
         
         String path = request.getRequestURI().substring(request.getContextPath().length());
         
+        if (path.equals("/") || path.isEmpty()) {
+            handleMvcRequest(request, response);
+            return;
+        }
+        
         boolean resourceExists = getServletContext().getResource(path) != null;
 
         if (resourceExists) {
