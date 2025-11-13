f37d5608fe13262e279ecc36893db0360e6ca98d
OrnellaMarion <168301329+NellaOne@users.noreply.github.com>
2025-11-13 10:06:11 +0300

    Sprint5
    envoie_data_controller_vue_jsp

--- Framework/src/main/java/mg/etu3273/framework/ModelView.java
+++ Framework/src/main/java/mg/etu3273/framework/ModelView.java
@@ -1,14 +1,28 @@
 package mg.etu3273.framework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModelView {
     
-    private String view;  
+    private String view; 
+    private Map<String, Object> data; 
     
     public ModelView() {
+        this.data = new HashMap<>();
     }
     
     public ModelView(String view) {
         this.view = view;
+        this.data = new HashMap<>();
+    }
+    
+    public void addObject(String key, Object value) {
+        this.data.put(key, value);
+    }
+
+    public Map<String, Object> getData() {
+        return data;
     }
     
     public String getView() {
@@ -19,8 +33,8 @@ public class ModelView {
         this.view = view;
     }
     
-    @Override
+   @Override
     public String toString() {
-        return "ModelView{view='" + view + "'}";
+        return "ModelView{view='" + view + "', data=" + data.size() + " élément(s)}";
     }
 }
